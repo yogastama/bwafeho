491a022045a341b28dd78b1615435e01f74e64f0
yogastama <yogabagas69@gmail.com>
2021-02-01 20:04:02 +0700

    belajar : - ternyata ada setup buat buka langsung ke browser saat ngejalanin development - cara kerja webpack mulai dari prod, dan buil development

--- configs/webpack.dev.config.js
+++ configs/webpack.dev.config.js
@@ -1,20 +1,31 @@
+// kalo ngejalanin development mode atau yang menjalankan file ini di packagejson, maka skrip ini akan dijalankan dengan kode berikut 
+
+// load webpack merge
+// berfungsi untuk menggabungkan webpack.config.js dengan file ini
 /* eslint-disable import/no-extraneous-dependencies */
 const { merge } = require("webpack-merge");
 
+// ambil webpack confignya
 const webpackConfiguration = require("../webpack.config");
+// load file env nya
 const environment = require("./env");
-
+// gabungin webpack utama dengan settingan webpack ini
 module.exports = merge(webpackConfiguration, {
+  // mode jadi development
   mode: "development",
 
   /* Manage source maps generation process */
+  // eval source map ini berfungsi untuk memudahkan untuk melihat kodingan mode development walaupun sudah dibuild
   devtool: "eval-source-map",
 
   /* Development Server Configuration */
+  // nyalakan devserver agar terbuka development server nya
   devServer: {
     contentBase: environment.paths.output,
     watchContentBase: true,
+    // path yang akan di load
     publicPath: "/",
+    // open langsung in browser : true
     open: true,
     historyApiFallback: true,
     compress: true,
@@ -23,10 +34,12 @@ module.exports = merge(webpackConfiguration, {
     watchOptions: {
       poll: 300,
     },
+    // load setup server di env
     ...environment.server,
   },
 
   /* File watcher options */
+  // kode dibawah fungsinya buat mengabaikan folder node_modules walaupun ada perubahan, jadi ngga lama saat perubahan
   watchOptions: {
     aggregateTimeout: 300,
     poll: 300,
@@ -34,5 +47,6 @@ module.exports = merge(webpackConfiguration, {
   },
 
   /* Additional plugins configuration */
+  // untuk tambahan plugins
   plugins: [],
 });
--- configs/webpack.prod.config.js
+++ configs/webpack.prod.config.js
@@ -1,17 +1,23 @@
 /* eslint-disable import/no-extraneous-dependencies */
+// merge webpack utama dan production
 const { merge } = require("webpack-merge");
+// load minify css
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+// load terser buat minify js
 const TerserPlugin = require("terser-webpack-plugin");
-
+// load webpack uatama buat digabungkan
 const webpackConfiguration = require("../webpack.config");
 
 module.exports = merge(webpackConfiguration, {
+  // mode producttion
   mode: "production",
 
   /* Manage source maps generation process. Refer to https://webpack.js.org/configuration/devtool/#production */
+  // devtool nya udah dimatiin lagi jadi biar ngga ada space dan tabs
   devtool: false,
 
   /* Optimization configuration */
+  // optimasi js dan css
   optimization: {
     minimize: true,
     minimizer: [
@@ -29,5 +35,6 @@ module.exports = merge(webpackConfiguration, {
   },
 
   /* Additional plugins configuration */
+  // tambahan plugins saat mau dijalankan
   plugins: [],
 });
